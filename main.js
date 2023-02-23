4fba5da53fb83b75dcba5bd8f5af2cd4df89e043
Ali Kone <tlfmcooper@gmail.com>
2023-02-22 19:02:48 -0600

    added crawlPage function

--- main.js
+++ main.js
@@ -13,9 +13,11 @@ async function main() {
 
     const baseURL = process.argv[2];
     console.log("Reading file " + process.argv[2]);
-    const text  = await crawlPage(baseURL);
+    const pages  = await crawlPage(baseURL, baseURL, {});
 
-    console.log(`${text}`);
+    for (const page of Object.entries(pages)) {
+        console.log(page);
+    }
 }
 
 main();
